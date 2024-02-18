a4bdce94b8d6d6def2210307332666aa161404e0
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-18 17:12:22 -0500

    fix typo second to milSecond

--- timer1.js
+++ timer1.js
@@ -1,11 +1,11 @@
 const args = process.argv.slice(2);
 function alarmClock(times) {
   for (const time of times) {
-    const second = parseInt(time, 10);
-  if (isNaN(second) || second <= 0) {
+    const milSecond = parseInt(time, 10);
+  if (isNaN(milSecond) || milSecond <= 0) {
     console.log('Invalid Input');
   } else {
-      setTimeout(() => require("child_process").exec("afplay /System/Library/Sounds/Glass.aiff"), second * 1000);
+      setTimeout(() => require("child_process").exec("afplay /System/Library/Sounds/Glass.aiff"), milSecond * 1000);
     }
   }
 }
